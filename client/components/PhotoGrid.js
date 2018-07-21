2d87e65337d9993939f4ee91a927e0913713f22a
Andrew Kim <jeesoo2002@gmail.com>
2018-07-20 22:05:17 -0700

    create store redux

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class PhotoGrid extends React.Component {
+  render() {
+    return <div className="photo-grid">I'm PhotoGrid</div>;
+  }
+}
+
+export default PhotoGrid;
